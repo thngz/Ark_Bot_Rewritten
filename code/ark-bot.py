c9f96ae5c4782d5e8556cba8ca8a6c78172363c2
Georg Kiviväli <54536253+GeorgK1@users.noreply.github.com>
2021-08-15 11:43:46 +0300

    Changed variable names

--- code/ark-bot.py
+++ code/ark-bot.py
@@ -81,7 +81,7 @@ class Arkbot:
     def tweet(self):
         load_dotenv()
         auth = tweepy.OAuthHandler(
-            os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET")
+            os.getenv("API_KEY"), os.getenv("API_SECRET_KEY")
         )
         auth.set_access_token(
             os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET")
